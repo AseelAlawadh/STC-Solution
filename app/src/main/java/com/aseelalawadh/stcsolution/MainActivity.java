cacb0ed86d49cce30b729f2026a4cd1c0312c1b8
Aseel AlAwadh <aseel.alawadh@gmail.com>
2018-04-30 13:57:43 +0300

    Code Solution to be part of STC team

---
+++ app/src/main/java/com/aseelalawadh/stcsolution/MainActivity.java
@@ -0,0 +1,35 @@
+package com.aseelalawadh.stcsolution;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.text_solution);
+        String input ="joinUs";
+        char [] word = input.toCharArray();
+        int length = input.length();
+        textView.setText(word.length +"");
+        for (int i=0; i<length; i++){
+            for (int j=i+1 ; j<length; j++){
+                if (word[j] < word[i]){
+                    char temp = word[i];
+                    word[i] = word[j] ;
+                    word[j] = temp ;
+                }
+            }
+        }
+
+        String email = String.valueOf(word);
+        email = email+"@stc.com.sa";
+        textView.setText(email);
+    }
+}
+
